146af83ac0a046d9329ffa1b34b4c0724282ac6c
Alex Lev <alexlev1013@gmail.com>
2019-03-13 14:52:03 -0500

    add floating button and commented code

--- Coms_362_Proj/app/src/main/java/com/example/project362/activities/EditInfoActivity.java
+++ Coms_362_Proj/app/src/main/java/com/example/project362/activities/EditInfoActivity.java
@@ -18,32 +18,43 @@ import com.google.firebase.auth.FirebaseAuth;
 
 public class EditInfoActivity extends AppCompatActivity implements View.OnClickListener {
 
+    //all of the variables
     EditText editEmail, editPass, editName, editPass2;
+    //button to submit the information
     Button editSub;
     private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        //get content
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_info);
 
+        //get the instances of the employee user authroization
         mAuth = FirebaseAuth.getInstance();
+
+        //get all of the views
         editName =  findViewById(R.id.editName);
         editEmail =  findViewById(R.id.editEmail);
         editPass =  findViewById(R.id.editPass);
         editPass2 =  findViewById(R.id.editPass2);
+
+        //get button
         findViewById(R.id.buttonSub).setOnClickListener(EditInfoActivity.this);
 
+        //get the email that the user used to register to edit it
         ((EditText) findViewById(R.id.editEmail)).setText(mAuth.getCurrentUser().getEmail());
 
     }
 
     private void updateUser() {
+        //get the input of what the employee wants to edit
         String name = editName.getText().toString().trim();
         String email = editEmail.getText().toString().trim();
         final String password = editPass.getText().toString().trim();
         String password2 = editPass2.getText().toString().trim();
 
+        //checks to see if there are errors and then records the errors
         if (name.isEmpty()) {
             editName.setError("Name is required");
             editName.requestFocus();
@@ -77,8 +88,7 @@ public class EditInfoActivity extends AppCompatActivity implements View.OnClickL
         }
         else
         {
-            //mAuth.getCurrentUser().updateEmail(email);
-            //mAuth.getCurrentUser().updatePassword(password);
+            //authorizes if the email and password is correct
             mAuth.getCurrentUser().updateEmail(email).addOnCompleteListener(new OnCompleteListener<Void>() {
                 @Override
                 public void onComplete(@NonNull Task<Void> task) {
@@ -99,29 +109,19 @@ public class EditInfoActivity extends AppCompatActivity implements View.OnClickL
                             }
                         });
                     }
+
+                    //shows errors
                     else{
                         Toast.makeText(getApplicationContext(),task.getException().getMessage(),Toast.LENGTH_SHORT).show();
                     }
                 }
             });
-            /*
-            mAuth.getCurrentUser().updatePassword(password).addOnCompleteListener(new OnCompleteListener<Void>() {
-                @Override
-                public void onComplete(@NonNull Task<Void> task) {
-                    if(task.isSuccessful()) {
-                        Toast.makeText(EditInfoActivity.this, "User Pass Succsesful", Toast.LENGTH_SHORT).show();
-                    }
-                    else{
-                        Toast.makeText(getApplicationContext(),task.getException().getMessage(),Toast.LENGTH_SHORT).show();
-                    }
-                }
-            });
-            */
         }
 
 
     }
 
+    //button to submit and save edits
     @Override
     public void onClick(View view){
         switch(view.getId()){
--- Coms_362_Proj/app/src/main/java/com/example/project362/activities/HomeActivity.java
+++ Coms_362_Proj/app/src/main/java/com/example/project362/activities/HomeActivity.java
@@ -1,11 +1,12 @@
 package com.example.project362.activities;
 
+import android.content.Intent;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
+import android.view.View;
 import android.widget.Toast;
 
 import com.example.project362.R;
@@ -15,15 +16,12 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.Query;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
-import java.util.Map;
-import java.util.*;
 
-public class HomeActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity implements View.OnClickListener {
 
     private RecyclerView recyclerView;
     private RecyclerView.Adapter mAdapter;
@@ -43,6 +41,10 @@ public class HomeActivity extends AppCompatActivity {
         // use this setting to improve performance if you know that changes
         // in content do not change the layout size of the RecyclerView
         recyclerView.setHasFixedSize(true);
+
+        //get onclikc for floating action button
+        findViewById(R.id.floatingActionButtonEdit).setOnClickListener(HomeActivity.this);
+
 
         // use a linear layout manager
         layoutManager = new LinearLayoutManager(this);
@@ -78,5 +80,16 @@ public class HomeActivity extends AppCompatActivity {
 
 
 
+    }
+
+    @Override
+    public void onClick(View view){
+        switch(view.getId()){
+            case R.id.floatingActionButtonEdit:
+                finish();
+                Intent i = new Intent(this, EditInfoActivity.class);
+                startActivity(i);
+                break;
+        }
     }
 }
