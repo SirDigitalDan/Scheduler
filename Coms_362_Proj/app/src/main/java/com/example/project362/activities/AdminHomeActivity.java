317c2b04314bb82b34f9f9c9e6c6e97b102e81cd
Unknown <daniellllev13@gmail.com>
2019-03-13 22:45:29 -0500

    Delete Employee + Create new Admin out of Employee
    This works from the admin view therefore the use case is complete :)

--- Coms_362_Proj/app/src/main/java/com/example/project362/activities/AdminHomeActivity.java
+++ Coms_362_Proj/app/src/main/java/com/example/project362/activities/AdminHomeActivity.java
@@ -1,21 +1,15 @@
 package com.example.project362.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
 
 import com.example.project362.R;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.firebase.firestore.DocumentReference;
-import com.google.firebase.firestore.FirebaseFirestore;
 
-public class AdminHomeActivity extends AppCompatActivity {
+public class AdminHomeActivity extends AppCompatActivity implements View.OnClickListener {
 
     private static final String TAG = "AdminControls";
     Button promote;
@@ -29,6 +23,7 @@ public class AdminHomeActivity extends AppCompatActivity {
 
         ememail = findViewById(R.id.emailtopromote);
         promote = findViewById(R.id.promoteButton);
+        findViewById(R.id.adminStatusButton).setOnClickListener(AdminHomeActivity.this);
 
         promote.setOnClickListener(new View.OnClickListener(){
             @Override
@@ -38,5 +33,16 @@ public class AdminHomeActivity extends AppCompatActivity {
 
             }
         });
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch(view.getId()) {
+            case R.id.adminStatusButton:
+                finish();
+                Intent in = new Intent(AdminHomeActivity.this, AdminStatusActivity.class);
+                startActivity(in);
+                break;
+        }
     }
 }
--- Coms_362_Proj/app/src/main/java/com/example/project362/activities/EditInfoActivity.java
+++ Coms_362_Proj/app/src/main/java/com/example/project362/activities/EditInfoActivity.java
@@ -13,18 +13,9 @@ import android.widget.Toast;
 
 import com.example.project362.R;
 import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.firestore.CollectionReference;
-import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.QueryDocumentSnapshot;
-import com.google.firebase.firestore.QuerySnapshot;
-
-import java.util.HashMap;
-import java.util.Map;
 
 
 public class EditInfoActivity extends AppCompatActivity implements View.OnClickListener {
@@ -50,71 +41,15 @@ public class EditInfoActivity extends AppCompatActivity implements View.OnClickL
         editEmail =  findViewById(R.id.editEmail);
         editPass =  findViewById(R.id.editPass);
         editPass2 =  findViewById(R.id.editPass2);
-        userEmail = findViewById(R.id.deleteUser);
+
 
         //get button
         findViewById(R.id.buttonSub).setOnClickListener(EditInfoActivity.this);
         findViewById(R.id.buttonSignout).setOnClickListener(EditInfoActivity.this);
-        findViewById(R.id.buttonDelete).setOnClickListener(EditInfoActivity.this);
-        findViewById(R.id.buttonAdd).setOnClickListener(EditInfoActivity.this);
+        
         ((EditText) findViewById(R.id.editEmail)).setText(mAuth.getCurrentUser().getEmail());
 
     }
-    private void deleteUser(){
-
-        String em = userEmail.getText().toString().trim();
-
-        FirebaseFirestore db = FirebaseFirestore.getInstance();
-        final CollectionReference ref = db.collection("Employees");
-        DocumentReference em1 = ref.document(em);
-
-        em1.delete();
-
-
-    }
-    private void createUser()
-    {
-        final String ema = userEmail.getText().toString().trim();
-        final FirebaseFirestore db1 = FirebaseFirestore.getInstance().getInstance();
-        final CollectionReference ref = db1.collection("Employees");
-        DocumentReference em1 = ref.document(ema);
-
-        db1.collection("Employees")
-                .get()
-                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-                    @Override
-                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                        if (task.isSuccessful()) {
-                            for (QueryDocumentSnapshot document : task.getResult()) {
-                                if(document.getId().equals(ema))
-                                {
-                                    Map<String, Object> city = new HashMap<>();
-                                    city.put("email", ema);
-
-
-                                    db1.collection("Admins").document(ema)
-                                            .set(city)
-                                            .addOnSuccessListener(new OnSuccessListener<Void>() {
-                                                @Override
-                                                public void onSuccess(Void aVoid) {
-                                                    Toast.makeText(EditInfoActivity.this, "Admin Created", Toast.LENGTH_SHORT).show();
-                                                }
-                                            })
-                                            .addOnFailureListener(new OnFailureListener() {
-                                                @Override
-                                                public void onFailure(@NonNull Exception e) {
-                                                    Toast.makeText(EditInfoActivity.this, "Failed to create", Toast.LENGTH_SHORT).show();
-                                                }
-                                            });
-                                }
-                            }
-                        } else {
-                            Toast.makeText(EditInfoActivity.this, "Employee not found in the Database", Toast.LENGTH_SHORT).show();
-                        }
-                    }
-                });
-
-        }
 
 
 
@@ -210,14 +145,7 @@ public class EditInfoActivity extends AppCompatActivity implements View.OnClickL
                 startActivity(intent);
                 break;
 
-            case R.id.buttonDelete:
-                finish();
-                deleteUser();
-                break;
-            case R.id.buttonAdd:
-                finish();
-                createUser();
-                break;
+
 
         }
     }
