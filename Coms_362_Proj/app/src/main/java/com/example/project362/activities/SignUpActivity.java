140d4fac6bfcdacc82ac6ef7077e45c1ee8cc9e9
Connor Rust <rust485@gmail.com>
2019-03-28 10:11:49 -0500

    create home page

--- Coms_362_Proj/app/src/main/java/com/example/project362/activities/SignUpActivity.java
+++ Coms_362_Proj/app/src/main/java/com/example/project362/activities/SignUpActivity.java
@@ -2,7 +2,6 @@ package com.example.project362.activities;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Patterns;
 import android.view.View;
@@ -11,17 +10,9 @@ import android.widget.Toast;
 
 import com.example.project362.R;
 import com.example.project362.models.Employee;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.firestore.CollectionReference;
-import com.google.firebase.firestore.FirebaseFirestore;
-
-import java.util.HashMap;
-import java.util.Map;
 
 public class SignUpActivity extends AppCompatActivity implements View.OnClickListener
 {
@@ -83,7 +74,7 @@ public class SignUpActivity extends AppCompatActivity implements View.OnClickLis
 				Toast.makeText(SignUpActivity.this, "User Register Succsesful",
 						Toast.LENGTH_SHORT).show();
 
-				Intent intent = new Intent(SignUpActivity.this, HomeActivity.class);
+				Intent intent = new Intent(SignUpActivity.this, AllShiftsActivity.class);
 
 
 				intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
